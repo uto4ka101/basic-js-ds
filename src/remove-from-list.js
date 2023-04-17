528b48905d188d4c92d0c64744af0e5922206e63
Yuliya Osipava <74550722+uto4ka101@users.noreply.github.com>
2023-04-17 16:51:17 +0300

    feat: update 21

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const { ListNode } = require('../extensions/list-node.js');
+// const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,79 +22,24 @@ const { ListNode } = require('../extensions/list-node.js');
  *   }
  * }
  */
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  add(elem) {
-    this.head = elem;
-  }
-
-  removeAt(position) {
-    let current = this.head;
-    if (position === 0) {
-      this.head = current.next;
-    } else {
-      let prev = null;
-      let index = 0;
-
-      while (index < position) {
-        prev = current;
-        current = current.next;
-        index++;
-      }
-
-      prev.next = current.next;
-    }
-    return current.value;
-  }
-
-
-
-  remove(element) {
-    this.removeAt(this.indexOf(element));
-  }
-
-  indexOf(element) {
-    let current = this.head;
-    let index = 0;
-
-    while (current) {
-      if (current.value === element) {
-        return index;
-      }
-
-      current = current.next;
-      index++;
-    }
-
-    return -1;
-  }
-}
-
-let list = new LinkedList();
 function removeKFromList(l, k) {
-  let elem = l;
-  if (l.value === k) {
+  let curr = l;
+  while (curr.value === k) curr = curr.next;
+  list = curr;
 
-    elem = l.next;
-
-  }
-
-  console.log('l', elem)
-  list.add(elem);
-  console.log('1', list.indexOf(k))
-
-  function del() {
-    if (list.indexOf(k) != -1) {
-      list.remove(k);
-      if (list.indexOf(k) != -1) {
-        del();
+  if (curr) {
+    let prev = null;
+    while (curr.next) {
+      prev = curr;
+      curr = curr.next;
+      if (curr.value === k) {
+        prev.next = curr.next;
       }
     }
-    return elem;
-
+    return l;
   }
-  return del();
-
 }
+
+module.exports = {
+  removeKFromList
+};
