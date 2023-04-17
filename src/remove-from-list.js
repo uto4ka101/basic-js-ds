5609c19d129f6f726d5cabb77620d3669749c0bd
Yuliya Osipava <74550722+uto4ka101@users.noreply.github.com>
2023-04-17 16:46:52 +0300

    feat: update18

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  add(elem) {
+    this.head = elem;
+  }
+
+  removeAt(position) {
+    let current = this.head;
+    if (position === 0) {
+      this.head = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = current.next;
+    }
+    return current.value;
+  }
+
+
+
+  remove(element) {
+    this.removeAt(this.indexOf(element));
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === element) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
 }
 
-module.exports = {
-  removeKFromList
-};
+let list = new LinkedList();
+function removeKFromList(l, k) {
+  let elem = l;
+  if (l.value === k) {
+
+    elem = l.next;
+
+  }
+
+  console.log('l', elem)
+  list.add(elem);
+  console.log('1', list.indexOf(k))
+
+  function del() {
+    if (list.indexOf(k) != -1) {
+      list.remove(k);
+      if (list.indexOf(k) != -1) {
+        del();
+      }
+    }
+    return elem;
+
+  }
+  return del();
+
+}
