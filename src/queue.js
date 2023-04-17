7bb967318335e88f372d5a68fd6149324cefd31b
Yuliya Osipava <74550722+uto4ka101@users.noreply.github.com>
2023-04-17 16:53:23 +0300

    feat: update 24

--- src/queue.js
+++ src/queue.js
@@ -16,37 +16,27 @@ const { NotImplementedError } = require('../extensions/index.js');
 class Queue {
 
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+    this.node = null;
   }
 
   getUnderlyingList() {
-    let current = this.head;
-    //current = current.next;
-    return current;
+    return this.node;
   }
 
   enqueue(value) {
-    const node = new ListNode(value);
-
-    if (this.head) { // if the first Node exitsts
-      this.tail.next = node; // inserts the created node after the tail of our Queue
-      this.tail = node; // now the created node is the last node
-    } else { // if there are no nodes in the Queue
-      this.head = node; // the created node is a head 
-      this.tail = node // also the created node is a tail in Queue because it is single.
+    if (this.node) {
+      let curr = this.node;
+      while (curr.next) curr = curr.next;
+      curr.next = new ListNode(value);
+    } else {
+      this.node = new ListNode(value);
     }
-
-    this.length++; // increases the length of Queue by 1
   }
 
   dequeue() {
-    const current = this.head; // saves the link to the head which we need to remove
-    this.head = this.head.next; // moves the head link to the second Node in the Queue
-    this.length--; // decreaments the length of our Queue
-
-    return current.value; // returns the removed Node's value
+    let value = this.node.value;
+    this.node = this.node.next;
+    return value
   }
 }
 
